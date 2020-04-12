877d2df1da5acef5f278f7908ece4a98d8881fd0
Roberto idizarri <ridizarri@hexacta.com>
2020-04-12 20:41:21 -0300

    Video 166

--- web/src/index.ts
+++ web/src/index.ts
@@ -4,29 +4,6 @@ import axios from 'axios';
 
 // const user = new User({ name: 'Ramiro', age: 13, id: 2 });
 
-const user = new User({ age: 13, name: 'Ramiro' });
+const user = new User({ age: 42, name: 'Roberto' });
 
 user.save();
-
-// setTimeout(() => {
-//     console.log(user);
-// },4000)
-
-// user.on('change', () => {
-//     console.log('Change #1');
-// });
-// user.on('change', () => {
-//     console.log('Change #1');
-// });
-
-// user.on('save', () => {
-//     console.log('Save was triggered.');
-// });
-
-// user.trigger('save');
-
-// user.set({name: 'Octavio', age: 7});
-// user.set({ age: 9});
-
-
-// console.log(user.get('age'));
--- web/src/models/user.ts
+++ web/src/models/user.ts
@@ -1,24 +1,22 @@
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
-interface UserProps {
+export interface UserProps {
     id?: number;
     name?: string;
     age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
 
 export class User {
     public events: Eventing = new Eventing();
-    constructor(private data: UserProps){};
+    public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+    public attributes: Attributes<UserProps>;
 
-    get(propName: string) : (number | string) {
-        return this.data[propName];
+    constructor(attrs: UserProps) {
+        this.attributes = new Attributes<UserProps>(attrs);
     }
-    set(update: UserProps): void {
-        Object.assign(this.data, update);
-    }
-
-
-
 
 }
